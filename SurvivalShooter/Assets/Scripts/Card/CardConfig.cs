e9483954ac256673f4904a617a493e36c40e15aa
Aieylisyh <Aieylisyh@users.noreply.github.com>
2022-07-19 15:41:28 +0800

    wip

---
+++ SurvivalShooter/Assets/Scripts/Card/CardConfig.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CardConfig : ScriptableObject
+{
+    public string id;
+
+    public int price;
+    public CardTier tier;
+    public CardCategory category;
+    public string title;
+    public string desc;
+
+    public CardParam param;
+    public CardParam goldenParam;
+}
+
+[System.Serializable]
+public class CardParam
+{
+    public int p1;
+    public int p2;
+}
+
