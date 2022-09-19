7dee4c1eb049aadaa81b1cf8fae688f06ecf62d9
QIYE7777 <931938629@qq.com>
2022-09-19 21:49:29 +0800

    Create SlowSpeed.cs

---
+++ SurvivalShooter/Assets/Scripts/Enemy/Skill/SlowSpeed.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class SlowSpeed : MonoBehaviour
+{
+    public GameObject character;
+    public float slowDown = 3;
+    private float speedTimestamp;
+    public float slowRate = 1.5f;
+
+    PlayerMovement playerMovement;
+
+    private void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    public void Slow()
+    {
+        playerMovement.speed  =- slowDown;
+        if (Time.time - speedTimestamp > slowRate)
+        {
+            playerMovement.speed =+ slowDown;
+        }
+    }
+    /*
+    private void Start()
+    {
+        speedTimestamp = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time > speedTimestamp)
+        {
+            speedTimestamp += slowRate;
+
+            Slow();
+        }
+    }
+    */
+}
